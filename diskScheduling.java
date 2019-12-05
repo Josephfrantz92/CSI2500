bc3991fbfa36be880b8035570b167498d0a1ebb5
Brandon Scoppa <scoppa.brandon@gmail.com>
2019-12-05 15:53:49 -0500

    Fixed indentation on SSTF algorithm

--- diskScheduling.java
+++ diskScheduling.java
@@ -8,7 +8,7 @@ public class diskScheduling {
         int userAlgorithm = Integer.parseInt(args[0]);
         System.out.println("/////Debug stuff/////");
         System.out.println("User picked: " + userAlgorithm); //User's choice of Algorithm
-        
+
         //Import scheduling file
         File f = new File(args[1]);
 
@@ -25,7 +25,7 @@ public class diskScheduling {
             //Variables used for algorithms
             Integer startPos = Integer.parseInt(list.get(0)); //Variable for starting position
             Integer direction = Integer.parseInt(list.get(1)); //Variable for direction
-            String requests = list.get(2); 
+            String requests = list.get(2);
             String[] requestArray = requests.split(" "); //Array for requests *Example requestArray[0] *
 
 
@@ -33,57 +33,59 @@ public class diskScheduling {
                 if (userAlgorithm == 0){
                     System.out.println("Algorithm: FCFS");
                 }
-                else if (userAlgorithm == 1){ for(int i=0; i<qSize;i++){
-            queue[i] = Integer.parseInt(input.nextLine());
-        }
+                else if (userAlgorithm == 1){
+                    for(int i=0; i<qSize;i++){
+                        queue[i] = Integer.parseInt(input.nextLine());
+                    }
 
-        //System.out.println("Enter initial head position");
-        head = Integer.parseInt(input.nextLine());
+                    //System.out.println("Enter initial head position");
+                    head = Integer.parseInt(input.nextLine());
 
-        for(int i=0; i<qSize; i++){
-            queue2[i] = Math.abs(head-queue[i]);
-        }
+                    for(int i=0; i<qSize; i++){
+                        queue2[i] = Math.abs(head-queue[i]);
+                    }
 
-        for(int i=0; i<qSize; i++){
+                    for(int i=0; i<qSize; i++){
 
-            for(int j=i+1; j<qSize; j++){
+                        for(int j=i+1; j<qSize; j++){
 
-                if(queue2[i] >queue2[j]){
-                    temp = queue2[i];
-                    queue2[i] = queue2[j];
-                    queue2[j] = temp;
+                            if(queue2[i] >queue2[j]){
+                                temp = queue2[i];
+                                queue2[i] = queue2[j];
+                                queue2[j] = temp;
 
-                    temp = queue[i];
-                    queue[i] = queue[j];
-                    queue[j] = temp;
+                                temp = queue[i];
+                                queue[i] = queue[j];
+                                queue[j] = temp;
+                            }
+
+                        }
+                    }
+
+                    for(int i=1; i<qSize; i++){
+                        seek = seek + Math.abs(head-queue[i]);
+                        //head = queue[i];
+                    }
+
+                    System.out.println("Total seek time is " +seek);
+                    //avg = seek/(float)qSize;
+                    //System.out.println("Average seek time is " +avg);
+
                 }
-
-            }
-        }
-
-        for(int i=1; i<qSize; i++){
-            seek = seek + Math.abs(head-queue[i]);
-            //head = queue[i];
-        }
-
-        System.out.println("Total seek time is " +seek);
-        //avg = seek/(float)qSize;
-        //System.out.println("Average seek time is " +avg);
-
-    }
-                    System.out.println("Algorithm: SSTF");
-                    System.out.println("Starting position: " + startPos); 
-                    System.out.println("Direction: " + direction);
-                    //System.out.println(requestArray[9]);
-                }
-                else if (userAlgorithm == 2){
-                    System.out.println("Algorithm: LOOK"); 
-                }
-                else if (userAlgorithm == 3){
-                    System.out.println("Algorithm: C-LOOK");    
-                }
+                                System.out.println("Algorithm: SSTF");
+                                System.out.println("Starting position: " + startPos);
+                                System.out.println("Direction: " + direction);
+                                //System.out.println(requestArray[9]);
+                            }
+                            else if (userAlgorithm == 2){
+                                System.out.println("Algorithm: LOOK");
+                            }
+                            else if (userAlgorithm == 3){
+                                System.out.println("Algorithm: C-LOOK");
+                            }
 
         }
+        
         catch (FileNotFoundException ex)  
         {
             System.out.println("My code sucks"); // insert code to run when exception occurs
