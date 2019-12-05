597ac43f08ee6476e3e58c66c4565d9ec0bc20d1
tovelbowie <47397176+tovelbowie@users.noreply.github.com>
2019-12-05 08:47:30 -0500

    Update diskScheduling.java
    Added the SSTF algorithm

--- diskScheduling.java
+++ diskScheduling.java
@@ -33,7 +33,44 @@ public class diskScheduling {
                 if (userAlgorithm == 0){
                     System.out.println("Algorithm: FCFS");
                 }
-                else if (userAlgorithm == 1){
+                else if (userAlgorithm == 1){ for(int i=0; i<qSize;i++){
+            queue[i] = Integer.parseInt(input.nextLine());
+        }
+
+        //System.out.println("Enter initial head position");
+        head = Integer.parseInt(input.nextLine());
+
+        for(int i=0; i<qSize; i++){
+            queue2[i] = Math.abs(head-queue[i]);
+        }
+
+        for(int i=0; i<qSize; i++){
+
+            for(int j=i+1; j<qSize; j++){
+
+                if(queue2[i] >queue2[j]){
+                    temp = queue2[i];
+                    queue2[i] = queue2[j];
+                    queue2[j] = temp;
+
+                    temp = queue[i];
+                    queue[i] = queue[j];
+                    queue[j] = temp;
+                }
+
+            }
+        }
+
+        for(int i=1; i<qSize; i++){
+            seek = seek + Math.abs(head-queue[i]);
+            //head = queue[i];
+        }
+
+        System.out.println("Total seek time is " +seek);
+        //avg = seek/(float)qSize;
+        //System.out.println("Average seek time is " +avg);
+
+    }
                     System.out.println("Algorithm: SSTF");
                     System.out.println("Starting position: " + startPos); 
                     System.out.println("Direction: " + direction);
