8a9a40ed338fed6d6adae853fd21b91d3dec3392
tovelbowie <47397176+tovelbowie@users.noreply.github.com>
2019-12-05 21:44:11 -0500

    Added SSTF algorithm

--- diskScheduling.java
+++ diskScheduling.java
@@ -41,7 +41,90 @@ public class diskScheduling {
                         System.out.println("Total head movement is: " + seek);
                 }
                 else if (userAlgorithm == 1){
-                    System.out.println("Algorithm: SSTF"); 
+                    System.out.println("Algorithm: SSTF");
+                    class pos { 
+      
+    // This variable is for the difference between head position and next number 
+    int dist = 0;  
+      
+    // This will be true if next number has been recieved 
+    boolean accessed = false;  
+}
+
+public class SSTF {
+// Calculates difference of each number from the head position 
+    public static void calcDifference(int queue[],int head, pos diff[]) 
+            
+    { 
+        for (int i = 0; i < diff.length; i++) 
+            diff[i].dist = Math.abs(queue[i] - head); 
+    } 
+  
+    // This finds new number at minimum distance from head 
+    public static int Min(pos diff[]) 
+    { 
+        int start = -1, minimum = Integer.MAX_VALUE; 
+  
+        for (int i = 0; i < diff.length; i++) { 
+            if (!diff[i].accessed && minimum > diff[i].dist) { 
+                  
+                minimum = diff[i].dist; 
+                start = i; 
+            } 
+        } 
+        return start; 
+    } 
+  
+    public static void SSTF(int request[], int head) 
+                                                       
+    { 
+        if (request.length == 0) 
+            return; 
+              
+        // creates array from pos. class     
+        pos diff[] = new pos[request.length];  
+          
+        // initializes array 
+        for (int i = 0; i < diff.length; i++)  
+          
+            diff[i] = new pos(); 
+          
+        // Seek operations counter 
+        int seek_count = 0;  
+          
+        // Stores disk access sequence
+        int[] seek_sequence = new int[request.length + 1];  
+          
+        for (int i = 0; i < request.length; i++) { 
+              
+            seek_sequence[i] = head; 
+            calcDifference(request, head, diff); 
+              
+            int index = Min(diff); 
+              
+            diff[index].accessed = true; 
+              
+            // total count increase
+            seek_count += diff[index].dist;  
+              
+            // next number is now new "head" 
+            head = request[index];  
+        } 
+          
+        // for last number 
+        seek_sequence[seek_sequence.length - 1] = head;  
+          
+        System.out.println("Total number of seek operations = " + seek_count);  
+    } 
+  
+    //public static void main(String[] args) 
+   // { 
+        // request array 
+       // int arr[] = { 131, 14, 43, 97, 72, 105, 104, 81, 116, 39, 86, 112, 93, 24, 58, 141, 135, 155, 50, 71 };  
+        //SSTF(arr, 122); 
+   // }
+    
+}
                 }
                 else if (userAlgorithm == 2){
                     System.out.println("Algorithm: LOOK"); 
